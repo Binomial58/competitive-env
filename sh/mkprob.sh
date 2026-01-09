ec47d51ed1d2ada4d8cd0565f08e206995628dbe
Binomial58 <gshuangle@gmail.com>
2026-01-09 18:39:27 +0900

    c++テンプレートの改善

--- sh/mkprob.sh
+++ sh/mkprob.sh
@@ -33,6 +33,17 @@ case "$LANG" in
 #include <bits/stdc++.h>
 using namespace std;
 
+/* utility : mathematical modulo (always non-negative) */
+inline int mod(int k, int n)
+{
+    return (k % n + n) % n;
+}
+
+inline long long mod(long long k, long long n)
+{
+    return (k % n + n) % n;
+}
+
 int main() {
     
 }
