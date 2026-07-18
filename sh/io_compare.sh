5f4be0efc13bfc2c6c76ecdfb51bd966cb43ebaf
Binomial58 <gshuangle@gmail.com>
2026-07-19 01:14:55 +0900

    ioall の出力比較・サンプル解決ロジックを共有ファイルに切り出し
    normalize_output / outputs_match / resolve_sample_input を
    sh/io_compare.sh にまとめ、ioall から source する形に変更。
    pyall.sh も後続コミットで同じ共有ファイルを利用するよう変更する。
    挙動は変更なし。

---
+++ sh/io_compare.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# ioall / pyall.sh で共通の出力比較・サンプル解決ロジック。
+# resolve_sample_input は呼び出し元が SAMPLE_DIR を設定している前提。
+
+normalize_output() {
+    local file="$1"
+
+    LC_ALL=C perl -0777 -pe '
+        s/[\t\r ]+(?=\n|\z)//g;
+        s/\n*\z//;
+        $_ .= "\n" if length;
+    ' "$file"
+}
+
+outputs_match() {
+    local expected="$1"
+    local actual="$2"
+
+    if cmp -s "$expected" "$actual"; then
+        return 0
+    fi
+
+    cmp -s <(normalize_output "$expected") <(normalize_output "$actual")
+}
+
+resolve_sample_input() {
+    local idx="$1"
+    if [ ! -d "$SAMPLE_DIR" ]; then
+        return 1
+    fi
+
+    if [ -f "$SAMPLE_DIR/sample-${idx}.in" ]; then
+        echo "$SAMPLE_DIR/sample-${idx}.in"
+        return 0
+    fi
+
+    local infile base suffix
+    for infile in "$SAMPLE_DIR"/*.in; do
+        base="$(basename "${infile%.in}")"
+        if [[ "$base" =~ -([0-9]+)$ ]]; then
+            suffix="${BASH_REMATCH[1]}"
+            if [ $((10#$suffix)) -eq $((10#$idx)) ]; then
+                echo "$infile"
+                return 0
+            fi
+        fi
+    done
+    return 1
+}
